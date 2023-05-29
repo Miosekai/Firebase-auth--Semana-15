bae339fa7923cb8fe36dd4e2def61788f058fa0f
Miosekai <1010070539@u.icesi.edu.co>
2023-05-29 00:00:37 -0500

    first commit

---
+++ firebase-todo-list-master-nat/register.js
@@ -0,0 +1,32 @@
+import { createUserWithEmailAndPassword, browserLocalPersistence } from "firebase/auth";
+import { auth, db } from "./firebase.js"; 
+import { doc, setDoc } from "firebase/firestore";
+
+const email = document.querySelector('#correo');
+const name = document.querySelector('#nombre');
+const username = document.querySelector('#usuario');
+const password = document.querySelector('#contrasena');
+const btn = document.querySelector('#boton-registro');
+
+btn.addEventListener('click',async  () => {
+    try {
+        const userData = {
+            email: email.value,
+            username: username.value,
+            name: name.value
+        };
+        await auth.setPersistence(browserLocalPersistence);
+        const {user: registeredUser} = await createUserWithEmailAndPassword(auth, email.value, password.value);
+        const userDoc = doc(db, 'users', registeredUser.uid);
+        await setDoc(userDoc, userData);
+        window.localStorage.setItem('userData', JSON.stringify(userData));
+
+        alert('Usuario registrado');
+        window.location.href = '/todlist.html';
+    } catch (error) {
+        alert(error);
+    }
+});
+
+
+
